adba8cd3f9aa4fcb47f35270397f050ea4012afb
Anupkumar sheshrao koturwar <118750899+Anup-2002@users.noreply.github.com>
2026-06-27 17:06:06 +0530

    Update message_generator.py

--- modules/message_generator.py
+++ modules/message_generator.py
@@ -8,8 +8,6 @@ from openai import OpenAI
 load_dotenv(override=True)
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
 client = OpenAI(
     api_key=OPENAI_API_KEY
 )
